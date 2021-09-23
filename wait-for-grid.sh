31a88d63eebfbf714c65447698ccad1352e4e51f
serhatgunes <serhat.gunes.92@gmail.com>
2021-09-24 00:16:25 +0300

    minor

--- wait-for-grid.sh
+++ wait-for-grid.sh
@@ -7,7 +7,7 @@ cmd="$@"
 
 echo "Waiting for selenium hub server response"
 timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:4444)" != '200' ]]; do sleep 5; done' || false
-
+sleep 15 s
 
 >&2 echo "Selenium Hub is up - executing tests"
 exec $cmd
